49db6f65958ae538c9f5d87d9e249f7b45641599
Marcus Kida <marcus@kida.io>
2016-07-27 17:15:06 +1000

    chore(lib): remove comment from `SecureCredentials`

--- Locative/Lib/SecureCredentials/SecureCredentials.swift
+++ Locative/Lib/SecureCredentials/SecureCredentials.swift
@@ -1,11 +1,3 @@
-//
-//  SecureCredentials.swift
-//  Locative
-//
-//  Created by Marcus Kida on 26/03/2016.
-//  Copyright © 2016 Marcus Kida. All rights reserved.
-//
-
 import UIKit
 import KeychainAccess
 
