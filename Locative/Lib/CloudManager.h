c67e28d1c41cb4e119cd0f110bd6cc4cf603db07
Marcus Kida <marcus@kida.io>
2016-07-30 22:45:08 +1000

    chore(cloudmanager): add nullability annotations

--- Locative/Lib/CloudManager.h
+++ Locative/Lib/CloudManager.h
@@ -13,17 +13,17 @@ typedef enum {
 
 @interface CloudManager : NSObject
 
-- (instancetype)init __attribute__((unavailable("Please use `initWithSettings:` instead")));
-- (instancetype)initWithSettings:(Settings *)settings;
+- (nullable instancetype)init __attribute__((unavailable("Please use `initWithSettings:` instead")));
+- (nonnull instancetype)initWithSettings:(Settings * __nullable)settings;
 
-- (void) signupAccountWithUsername:(NSString *)username andEmail:(NSString *)email andPassword:(NSString *)password onFinish:(void(^)(NSError *error, CloudManagerSignupError gfcError))finish;
-- (void) loginToAccountWithUsername:(NSString *)username andPassword:(NSString *)password onFinish:(void(^)(NSError *error, NSString *sessionId))finish;
-- (void) checkSessionWithSessionId:(NSString *)sessionId onFinish:(void(^)(NSError *error))finish;
+- (void) signupAccountWithUsername:(NSString * __nonnull)username andEmail:(NSString *__nonnull)email andPassword:(NSString *__nonnull)password onFinish:(nullable void(^)(NSError *__nullable error, CloudManagerSignupError gfcError))finish;
+- (void) loginToAccountWithUsername:(NSString *__nonnull)username andPassword:(NSString *__nonnull)password onFinish:(nullable void(^)(NSError *__nullable error, NSString *__nullable sessionId))finish;
+- (void) checkSessionWithSessionId:(NSString *__nonnull)sessionId onFinish:(nullable void(^)(NSError *__nullable error))finish;
 
-- (void) dispatchCloudFencelog:(Fencelog *)fencelog onFinish:(void(^)(NSError *error))finish;
-- (void) validateSessionWithCallback:(void(^)(BOOL valid))cb;
+- (void) dispatchCloudFencelog:(Fencelog *__nonnull)fencelog onFinish:(nullable void(^)(NSError *__nullable error))finish;
+- (void) validateSessionWithCallback:(nullable void(^)(BOOL valid))cb;
 - (void) validateSession;
-- (void) loadGeofences:(void(^)(NSError *error, NSArray *geofences))completion;
-- (void) uploadGeofence:(Geofence *)geofence onFinish:(void(^)(NSError *error))finish;
+- (void) loadGeofences:(nullable void(^)(NSError *__nullable error, NSArray *__nullable geofences))completion;
+- (void) uploadGeofence:(Geofence *__nonnull)geofence onFinish:(nullable void(^)(NSError *__nullable error))finish;
 
 @end
