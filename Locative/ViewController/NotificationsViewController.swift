320fc2d215634c53bee7b91740d2cc98cf3eb0ba
Marcus Kida <marcus@kida.io>
2016-10-19 08:50:45 +1100

    feat(Notifications): Add NotificationsViewController

---
+++ Locative/ViewController/NotificationsViewController.swift
@@ -0,0 +1,11 @@
+import NMessenger
+
+class NotificationsViewContorller: NMessengerViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let content = ContentNode()
+//        content
+//        let message = MessageNode(content: <#T##ContentNode#>)
+    }
+}
